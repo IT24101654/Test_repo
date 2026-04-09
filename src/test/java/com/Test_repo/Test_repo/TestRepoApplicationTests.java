9ce68eabf4e268d03e894027c4cdf085906a6c61
IT24101654 <yasith.hasarinda2003@gmail.com>
2026-04-09 11:48:22 +0530

    folder structure

---
+++ src/test/java/com/Test_repo/Test_repo/TestRepoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Test_repo.Test_repo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestRepoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
